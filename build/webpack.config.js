8d721a1b3c4e8022899168303b5d5a44f9790cc8
unknown <18271683093>
2020-01-02 16:46:19 +0800

    v1.0.0   整合vue-webpack+express backend

---
+++ build/webpack.config.js
@@ -0,0 +1,4 @@
+const path = require('path');
+const merge = require('webpack-merge')
+const devWebpackConfig = require('./webpack.dev.conf')
+module.exports = devWebpackConfig
--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const multipart = require('connect-multiparty')
 const cookieParser = require('cookie-parser')
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware')
+
+const router = require('./backend/router.js')
+const webpackConfig = require('./build/webpack.dev.conf')
+
 const app = express()
 const port = process.env.PORT || 8080
-const router = require('./backend/router.js')
+const complier = webpack(webpackConfig)
 
+app.use(webpackDevMiddleware(complier, {
+    publicPath: webpackConfig.output.publicPath,
+}))
 // parse application/x-www-form-urlencoded  
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json  
@@ -17,9 +26,7 @@ app.use(cookieParser())
 
 
 app.use('/api', router)
-app.get('/', (req, res) => {
-    res.end('index')
-})
+
 app.listen(port, () => {
     console.log(`server started at localhost:${port}`)
 })
